b5e4d86f431d627a692a83063903f9cab5992d95
melon <meloniusz@hotmail.com>
2020-01-04 13:54:52 +0800

    change to html routinh - fix

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,10 @@
 let path = require("path");
 
 module.exports = function(app) {
-  app.get("../survey.html", function(req, res) {
+  app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/survey.html"));
   });
+
   app.use(function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
